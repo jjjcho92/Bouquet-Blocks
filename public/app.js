672cc467aa9530291f7466e8ae55782ed1e2055c
jchobanana <jchobanana@gmail.com>
2022-11-06 00:53:41 +0800

    Draggable Test, Problem with Circles

--- public/app.js
+++ public/app.js
@@ -1,11 +1,12 @@
 // connects p5
 
-const container = document.querySelector ("#canvas-container");
+const container = document.querySelector("#canvas-container");
 // console.log(container.clientWidth);
 
-//Testing, Creating the 2 square shapes 
-let shape1;
-let shape2;
+
+//I switched to another Draggble Example: https://editor.p5js.org/enickles/sketches/HJoISR_WM 
+let rectangles = [];
+let circles = [];
 
 function setup() {
 	//Create the canvas and save it to a variable;
@@ -13,18 +14,32 @@ function setup() {
 	//Set the parent of the canvas to an exisitng html element's id value
 	myCanvas.parent("canvas-container");
 
-	shape1 = new Draggable(100, 100, 50, 50);
-	shape2 = new Draggable(200, 100, 50, 50);
+	for (let i = 0; i < 8; i++) {
+		let x = random(width);
+		let y = random(height);
+		let w = random(40, 80);
+		let h = random(40, 80);
+		rectangles[i] = new Rectangle(x, y, w, h);
+	}
+
+	for (let i = 0; i < 6; i++) {
+		let x = random(width);
+		let y = random(height);
+		let d = random(40, 80);
+		circles[i] = new Circle(x, y, d);
+	}
 }
 
 function draw() {
 	background(220, 40, 50);
-	shape1.over();
-	shape1.update();
-	shape1.show();
-	shape2.over();
-	shape2.update();
-	shape2.show();
+
+	for (i = 0; i < rectangles.length; i++) {
+		rectangles[i].show(mouseX, mouseY);
+	}
+
+	for (i = 0; i < circles.length; i++) {
+		circles[i].show(mouseX, mouseY);
+	}
 }
 
 // function mouseMoved() {
@@ -34,76 +49,100 @@ function draw() {
 // }
 
 function mousePressed() {
-	shape1.pressed();
-	shape2.pressed();
-  }
-  
-  function mouseReleased() {
-	shape1.released();
-	shape2.released();
-  }
+
+	for (i = 0; i < rectangles.length; i++) {
+		rectangles[i].pressed(mouseX, mouseY);
+	}
+
+	for (i = 0; i < circles.length; i++) {
+		circles[i].pressed(mouseX, mouseY);
+	}
+}
+
+function mouseReleased() {
+	for (i = 0; i < rectangles.length; i++) {
+		rectangles[i].notPressed();
+	}
+
+	for (i = 0; i < circles.length; i++) {
+		circles[i].notPressed();
+	}
+}
 
 function windowResized() {
-    resizeCanvas(container.clientWidth, container.clientHeight);
-    background(220, 40, 50);
+	resizeCanvas(container.clientWidth, container.clientHeight);
+	background(220, 40, 50);
 }
 
 
-//Class from Draggable Tutorial from link Nun shared 
-class Draggable {
+//Class for all the rectangles and squares
+class Rectangle {
 	constructor(x, y, w, h) {
-	  this.dragging = false; // Is the object being dragged?
-	  this.rollover = false; // Is the mouse over the ellipse?
-	  this.x = x;
-	  this.y = y;
-	  this.w = w;
-	  this.h = h;
-	  this.offsetX = 0;
-	  this.offsetY = 0;
-	}
-  
-	over() {
-	  // Is mouse over object
-	  if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-		this.rollover = true;
-	  } else {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.offsetX = 0;
+		this.offsetY = 0;
+		this.dragging = false;
 		this.rollover = false;
-	  }
 	}
-  
-	update() {
-	  // Adjust location if being dragged
-	  if (this.dragging) {
-		this.x = mouseX + this.offsetX;
-		this.y = mouseY + this.offsetY;
-	  }
+
+	show(px, py) {
+		if (this.dragging) {
+			this.x = px + this.offsetX;
+			this.y = py + this.offsetY;
+		}
+
+		noStroke();
+		fill(252, 186, 3);
+		rect(this.x, this.y, this.w, this.h);
 	}
-  
-	show() {
-	  stroke(0);
-	  // Different fill based on state
-	  if (this.dragging) {
-		fill(50);
-	  } else if (this.rollover) {
-		fill(100);
-	  } else {
-		fill(175, 200);
-	  }
-	  rect(this.x, this.y, this.w, this.h);
+
+	pressed(px, py) {
+		if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
+			// print("clicked on rect");
+			this.dragging = true;
+			this.offsetX = this.x - px;
+			this.offsetY = this.y - py;
+		}
 	}
-  
-	pressed() {
-	  // Did I click on the rectangle?
-	  if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
-		this.dragging = true;
-		// If so, keep track of relative location of click to corner of rectangle
-		this.offsetX = this.x - mouseX;
-		this.offsetY = this.y - mouseY;
-	  }
+	notPressed() {
+		this.dragging = false;
 	}
-  
-	released() {
-	  // Quit dragging
-	  this.dragging = false;
+}
+
+class Circle {
+	constructor(x, y, d) {
+		this.x = x;
+		this.y = y;
+		this.d = d;
+		this.offsetX = 0;
+		this.offsetY = 0;
+		this.dragging = false;
+		this.rollover = false;
 	}
-  }
+
+	show(px, py) {
+		if (this.dragging) {
+			this.x = px + this.offsetX;
+			this.y = py + this.offsetY;
+		}
+
+		noStroke();
+		fill(0, 173, 46);
+		circle(this.x, this.y, this.d);
+	}
+
+	pressed(px, py) {
+		if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
+			// print("clicked on rect");
+			this.dragging = true;
+			this.offsetX = this.x - px;
+			this.offsetY = this.y - py;
+		}
+	}
+	notPressed() {
+		this.dragging = false;
+	}
+}
