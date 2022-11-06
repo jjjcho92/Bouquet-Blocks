00074327f188794d739369b7d33e6838ce09ec2b
Kat <kp3038@nyu.edu>
2022-11-06 13:10:50 -0800

    beginning of the back end

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express")
 const app = express()
 const port = 3000
+// installed npm i nedb (this gives you the .db file)
+// reference module
+const DataStore = require("nedb")
+// line 8-9 is boilerplate for using nedb
+const db = new DataStore ("gallery.db")
+db.loadDatabase ()
+// use express.json lets us read the json (parse json back to its original form)
+app.use(express.json({
+    limit: "50mb"
+}))
 
 app.use ("/", express.static("public"))
+
+// making a post request so we want our app to "listen" for this post request
+app.post ("/submit", (req, res) => {
+    // console.log(req)
+    // boilerplate (req.body is the "body" of your request)
+    db.insert (req.body)
+})
+
+app.get ("/gallery", (req, res) => {
+    // https://github.com/louischatriot/nedb#finding-documents
+    db.find ({}, function (err, docs) {
+    // console.log(docs)
+    let obj = {data: docs}
+    res.json(obj)
+    })
+})
+
 app.listen (port, function(){
     console.log ("listening on port 3000")
 })
--- public/app.js
+++ public/app.js
@@ -3,7 +3,6 @@
 const container = document.querySelector("#canvas-container");
 // console.log(container.clientWidth);
 
-
 //I take out the circles for now as I'm not able to make it work. Working on p5 editor seperately: https://editor.p5js.org/jjjcho92/sketches/6mcrYaJrw 
 let rectangles = [];
 
@@ -93,3 +92,37 @@ class Rectangle {
 		this.dragging = false;
 	}
 }
+
+const submit = document.querySelector(".button");
+
+submit.addEventListener("click", function(){
+	// capture canvas (1. refer to canvas in HTML, 2. captures image in form of text 3. storing as a value in obj)
+	const capture = document.querySelector("canvas")
+	const imageData = capture.toDataURL("image/jpeg")
+	let obj = {image:imageData} 
+	// console.log (imageData)
+	// send to server (1. post data to a webpage 2. sending body (as JSON) 3. turning into JSON string)
+	fetch("/submit", {
+		method:"POST",
+		headers: {
+			"Content-Type": "application/json;charset=utf-8",
+		},
+		body:JSON.stringify (obj),
+	}).then((response) => response.json)
+		.then((data)=> {
+		// console.log(data)
+	})
+	// store in database
+})
+
+const display = document.querySelector(".display");
+display.addEventListener("click", function(){
+	fetch("/gallery").then((response) => response.json())
+	.then((data) => {
+		// console.log(data.data[0].image)
+		const gallery = document.querySelector(".container");
+		const imageElem = document.createElement("img")
+		imageElem.src = data.data[0].image
+		gallery.appendChild (imageElem)
+	}) 
+})
